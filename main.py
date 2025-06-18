7cc2b7a410d7a7ee808edbf9d74bc2c981e03450
IT21314742 <it21314742@my.sliit.lk>
2025-06-18 19:53:22 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
     
     model = load_cached_model()
     
-    uploaded_file = st.file_uploader()
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
     
 
 
